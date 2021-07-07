22cdeefd6e6fa84ec3f717243aa57dfcd1002768
Catalano Nicolas <nec.catalano@gmail.com>
2021-07-07 20:25:37 -0300

    Creación de modelo post y corrección de url de las imagenes

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -1,20 +1,8 @@
-"""platzigram URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# Django
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from platzigram import views as local_views
 from posts import views as posts_views
 
@@ -24,4 +12,4 @@ urlpatterns = [
     path('sorted/', local_views.sorted),
     path('hi/<str:name>/<int:age>', local_views.say_hi),
     path('posts', posts_views.list_posts),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- posts/models.py
+++ posts/models.py
@@ -1,9 +1,9 @@
 """ Posts models. """
+
 # Django
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
 
 class Post(models.Model):
     """ Post model. """
@@ -17,6 +17,7 @@ class Post(models.Model):
     )
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile = models.ForeignKey('users.Profile', on_delete=models.CASCADE)
 
     # Cuando se crea el registro le asigna el valor de manera auto
     created = models.DateTimeField(auto_now_add=True)  
@@ -24,5 +25,5 @@ class Post(models.Model):
     modified = models.DateTimeField(auto_now=True)
 
     def __str__(slef):
-        return 
-
+        """ Return title and username. """
+        return f"{self.title} by @{self.user.username}"
