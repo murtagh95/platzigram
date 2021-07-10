5b1394e12a3e00f20ac5e24ba54b1b7ee18ab706
Catalano Nicolas <nec.catalano@gmail.com>
2021-07-10 14:40:06 -0300

    Creación de middleware que verifica que el usuario tenga foto de perfil y biografia

---
+++ platzigram/middleware.py
@@ -0,0 +1,33 @@
+""" Platzigram middleware catalog. """ 
+
+# Django
+from django.shortcuts import redirect
+from django.urls import reverse
+
+class ProfileCompletionMiddleware:
+    """ Profile completion middleware. 
+    Ensure every user that is interacting with the platform
+    have their profile picture and biography.
+    """
+    def __init__(self, get_response):
+        """ Middleware initialization.  """
+        self.get_response = get_response
+    
+
+    def __call__(self, request):
+        """ Code to be execute for each request before the
+        view is called. """
+        # Verifico que este logeado el usuario
+        if not request.user.is_anonymous:
+            profile = request.user.profile
+            if not profile.picture or not profile.biography:
+                # Verifico que no se encuentre en la vista para
+                # actualizar el perfil sino se queda en un bucle infi
+                if request.path not in [
+                    reverse('update_profile'),
+                    reverse('logout')
+                ] :
+                    return redirect("update_profile")
+        
+        response = self.get_response(request)
+        return response 
--- platzigram/settings.py
+++ platzigram/settings.py
@@ -60,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'platzigram.middleware.ProfileCompletionMiddleware'
 ]
 
 ROOT_URLCONF = 'platzigram.urls'
