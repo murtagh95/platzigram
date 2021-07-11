1ec506d24842683e36c4745faa842da2157fb60e
Catalano Nicolas <nec.catalano@gmail.com>
2021-07-11 15:20:04 -0300

    Modifico el update_profile para ver información de la clase Form

--- users/views.py
+++ users/views.py
@@ -75,6 +75,7 @@ def signup_view(request):
 
     return render(request, 'users/signup.html')
 
+@login_required
 def update_profile(request):
     """ Update a user's profile view. """
     profile = request.user.profile
